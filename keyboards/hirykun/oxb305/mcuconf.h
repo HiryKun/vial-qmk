0abce43a450cf9f1923993f44e05a8187924578d
HiryKun <1951086367@qq.com>
2024-09-25 18:39:01 +0800

    新增键盘oxb305

---
+++ keyboards/hirykun/oxb305/mcuconf.h
@@ -0,0 +1,4 @@
+#include_next <mcuconf.h>
+
+#undef STM32_PWM_USE_TIM2
+#define STM32_PWM_USE_TIM2 TRUE
---
+++ keyboards/hirykun/oxb305/oxb305.c
@@ -0,0 +1,10 @@
+#include QMK_KEYBOARD_H
+
+bool rgb_matrix_indicators_kb(void) {
+    if (!rgb_matrix_indicators_user()) {
+        return false;
+    }
+    if (host_keyboard_led_state().caps_lock) rgb_matrix_set_color(0, 255, 0, 0);
+    else rgb_matrix_set_color(0, 0, 0, 0);
+    return true;
+}
