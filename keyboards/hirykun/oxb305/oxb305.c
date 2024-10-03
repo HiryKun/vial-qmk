b5efe7b8141e9b65b8cbae744dcfde24ef1eec0a
HiryKun <1951086367@qq.com>
2024-10-03 23:48:27 +0800

    调整oxb305

--- keyboards/hirykun/oxb305/oxb305.c
+++ keyboards/hirykun/oxb305/oxb305.c
@@ -4,7 +4,7 @@ bool rgb_matrix_indicators_kb(void) {
     if (!rgb_matrix_indicators_user()) {
         return false;
     }
-    if (host_keyboard_led_state().caps_lock) rgb_matrix_set_color(0, 255, 0, 0);
+    if (host_keyboard_led_state().caps_lock) rgb_matrix_set_color(0, 255, 255, 255);
     else rgb_matrix_set_color(0, 0, 0, 0);
     return true;
 }
