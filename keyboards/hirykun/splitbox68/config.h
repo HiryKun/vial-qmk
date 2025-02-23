889cb7dc338e838adf41940eaab1cce4a2348760
HiryKun <1951086367@qq.com>
2025-02-23 23:33:53 +0800

    添加更改spilitbox68

---
+++ keyboards/hirykun/splitbox68/config.h
@@ -0,0 +1,9 @@
+//#define SPLIT_HAND_PIN B5 读取电平判断左右
+#define SOFT_SERIAL_PIN D2
+#define SPLIT_TRANSPORT_MIRROR
+#define RGBLIGHT_LAYERS
+//#define MASTER_LEFT usb只连接左边
+#define EE_HANDS //qmk指定左右手
+#define SPLIT_USB_DETECT //连接usb的部分被委派为主
+#define SPLIT_LED_STATE_ENABLE// 同步左右指示灯状态
+#define RGB_MATRIX_LED_COUNT 68
