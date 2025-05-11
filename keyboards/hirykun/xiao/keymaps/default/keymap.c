a75211aec92d7b4eddf5b49b51983a39f28d2eb6
HiryKun <1951086367@qq.com>
2025-05-11 10:25:11 +0800

    添加键盘“xiao”

---
+++ keyboards/hirykun/xiao/keymaps/default/keymap.c
@@ -0,0 +1,12 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   ,
+        KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN,
+        KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH
+    )
+};
